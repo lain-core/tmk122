44832aa9678c5a78f287e085391cf82439295c62
hunter king <hking0036@gmail.com>
2018-03-29 15:45:37 -0400

    cleanup repo, include readme.md

--- converter/terminal_usb/.dep/obj_terminal_lufa_common_util.o.d
+++
@@ -1,7 +0,0 @@
-obj_terminal_lufa/common/util.o: \
- /users/hunter/downloads/tmk_core/common/util.c config.h \
- /users/hunter/downloads/tmk_core/common/util.h
-
-config.h:
-
-/users/hunter/downloads/tmk_core/common/util.h:
--- converter/terminal_usb/.dep/obj_terminal_lufa_protocol_ps2_io_avr.o.d
+++
@@ -1,4 +0,0 @@
-obj_terminal_lufa/protocol/ps2_io_avr.o: \
- /users/hunter/downloads/tmk_core/protocol/ps2_io_avr.c config.h
-
-config.h:
